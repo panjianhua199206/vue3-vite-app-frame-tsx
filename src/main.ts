d11c5a477a376c8bc0abc9e5835b2de28bd566c4
潘建华 <1037543579@qq.com>
2020-10-08 11:28:15 +0800

    移除全局引用pan-slider.less

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,5 @@ import App from './App'
 import './index.css'
 import router from './router';
 import store from './store';
-import './components/pan-slider/pan-slider.less';
 
 createApp(App).use(router).use(store).mount('#app')
