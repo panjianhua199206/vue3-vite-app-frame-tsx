86744b833129040f2289f6d849bf83029345914a
潘建华 <1037543579@qq.com>
2020-10-08 11:27:42 +0800

    less 模块导入配置

--- src/shim.d.ts
+++ src/shim.d.ts
@@ -1,5 +1,10 @@
-// 这个文件其实不太需要，因为项目中全是通过tsx开发的
+// 这段代码其实不太需要，因为项目中全是通过tsx开发的
 declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
+}
+// less 模块导入配置
+declare module "*.less" {
+  const less: any;
+  export default less;
 }
